9e55d178ffe922b31405f1100fa25ed140357303
sanjaygurjar87 <41376205+sanjaygurjar87@users.noreply.github.com>
2020-04-23 21:15:08 +0100

    Updated

---
+++ PlayersController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Players_Service.Model;
+
+namespace Players_Service.Controllers
+{
+    [ApiController]
+    public class PlayersController : ControllerBase
+    {
+        [HttpGet]
+        [Route("api/Players/forgotpassword/{emailId}")]
+        public async Task<ActionResult<IEnumerable<string>>> GetPlayersCredentials(string emailId)
+        {
+            var playersModel = new PlayersModel();
+             playersModel.writeEmail(emailId);
+
+            return  Ok("Email Sent To Your MailID" + " " + emailId);
+           
+        }
+        [HttpGet]
+        [Route("api/Players/tempLogin/{emailId}")]
+        public async Task<ActionResult<IEnumerable<string>>> LoginByEmailAddress(string emailId)
+        {
+            return Ok("https://GreenTube.com/TempLogin");
+        }
+        
+    }
+}
---
+++ PlayersModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Players_Service.Model
+{
+    public class PlayersModel
+    {
+        string filePath = "";
+        public void writeEmail(string playersEmail)
+        {
+
+             filePath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            filePath = filePath + @"\Emails\";
+
+            if (!Directory.Exists(filePath))
+            {
+                Directory.CreateDirectory(filePath);
+            }
+
+            string text = @"Dear player,
+                          In order to log in to your account so you can change your password
+                          click the following link:
+
+                          < URL here pointing to the second endpoint described below>
+
+                          Note that this link is only valid for 1 hour, after which it expires!
+ 
+                         Happy playing!
+                         Greentube Support Team>";
+ 
+
+             File.WriteAllText(filePath+playersEmail+".txt", text);
+
+        }
+    }
+}
